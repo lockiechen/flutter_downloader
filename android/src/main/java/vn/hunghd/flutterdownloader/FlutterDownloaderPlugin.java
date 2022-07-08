9bbab2b0b979d76bf86d0ff7aae156571b23bc05
lockiechen <lockiechen@tencent.com>
2022-07-08 15:33:21 +0800

    add xlog

--- android/src/main/java/vn/hunghd/flutterdownloader/FlutterDownloaderPlugin.java
+++ android/src/main/java/vn/hunghd/flutterdownloader/FlutterDownloaderPlugin.java
@@ -34,6 +34,21 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 
+import com.elvishew.xlog.LogConfiguration;
+import com.elvishew.xlog.LogLevel;
+import com.elvishew.xlog.XLog;
+import com.elvishew.xlog.flattener.ClassicFlattener;
+import com.elvishew.xlog.interceptor.BlacklistTagsFilterInterceptor;
+import com.elvishew.xlog.libcat.LibCat;
+import com.elvishew.xlog.printer.AndroidPrinter;
+import com.elvishew.xlog.printer.Printer;
+import com.elvishew.xlog.printer.file.FilePrinter;
+import com.elvishew.xlog.printer.file.naming.DateFileNameGenerator;
+import com.elvishew.xlog.printer.file.writer.SimpleWriter;
+
+import java.io.File;
+
+
 public class FlutterDownloaderPlugin implements MethodCallHandler, FlutterPlugin {
     private static final String CHANNEL = "vn.hunghd/downloader";
     private static final String TAG = "flutter_download_task";
@@ -56,6 +71,7 @@ public class FlutterDownloaderPlugin implements MethodCallHandler, FlutterPlugin
             if (flutterChannel != null) {
                 return;
             }
+            initXlog();
             this.context = applicationContext;
             flutterChannel = new MethodChannel(messenger, CHANNEL);
             flutterChannel.setMethodCallHandler(this);
@@ -389,5 +405,63 @@ public class FlutterDownloaderPlugin implements MethodCallHandler, FlutterPlugin
             if (videoCursor != null) videoCursor.close();
         }
         if (imageCursor != null) imageCursor.close();
+    }
+
+    private void initXlog() {
+      LogConfiguration config = new LogConfiguration.Builder()
+          .logLevel(LogLevel.ALL)             // Specify log level, logs below this level won't be printed, default: LogLevel.ALL
+          .tag(getString(R.string.global_tag))                   // Specify TAG, default: "X-LOG"
+          // .enableThreadInfo()                                 // Enable thread info, disabled by default
+          // .enableStackTrace(2)                                // Enable stack trace info with depth 2, disabled by default
+          // .enableBorder()                                     // Enable border, disabled by default
+          // .jsonFormatter(new MyJsonFormatter())               // Default: DefaultJsonFormatter
+          // .xmlFormatter(new MyXmlFormatter())                 // Default: DefaultXmlFormatter
+          // .throwableFormatter(new MyThrowableFormatter())     // Default: DefaultThrowableFormatter
+          // .threadFormatter(new MyThreadFormatter())           // Default: DefaultThreadFormatter
+          // .stackTraceFormatter(new MyStackTraceFormatter())   // Default: DefaultStackTraceFormatter
+          // .borderFormatter(new MyBoardFormatter())            // Default: DefaultBorderFormatter
+          // .addObjectFormatter(AnyClass.class,                 // Add formatter for specific class of object
+          //     new AnyClassObjectFormatter())                  // Use Object.toString() by default
+          .addInterceptor(new BlacklistTagsFilterInterceptor(    // Add blacklist tags filter
+              "blacklist1", "blacklist2", "blacklist3"))
+          // .addInterceptor(new WhitelistTagsFilterInterceptor( // Add whitelist tags filter
+          //     "whitelist1", "whitelist2", "whitelist3"))
+          // .addInterceptor(new MyInterceptor())                // Add a log interceptor
+          .build();
+  
+      Printer androidPrinter = new AndroidPrinter();             // Printer that print the log using android.util.Log
+      Printer filePrinter = new FilePrinter                      // Printer that print the log to the file system
+          .Builder(new File(getExternalCacheDir().getAbsolutePath(), "devops-download-log").getPath())       // Specify the path to save log file
+          .fileNameGenerator(new DateFileNameGenerator())        // Default: ChangelessFileNameGenerator("log")
+          // .backupStrategy(new MyBackupStrategy())             // Default: FileSizeBackupStrategy(1024 * 1024)
+          // .cleanStrategy(new FileLastModifiedCleanStrategy(MAX_TIME))     // Default: NeverCleanStrategy()
+          .flattener(new ClassicFlattener())                     // Default: DefaultFlattener
+          .writer(new SimpleWriter() {                           // Default: SimpleWriter
+            @Override
+            public void onNewFileCreated(File file) {
+              super.onNewFileCreated(file);
+              final String header = "\n>>>>>>>>>>>>>>>> File Header >>>>>>>>>>>>>>>>" +
+                      "\nDevice Manufacturer: " + Build.MANUFACTURER +
+                      "\nDevice Model       : " + Build.MODEL +
+                      "\nAndroid Version    : " + Build.VERSION.RELEASE +
+                      "\nAndroid SDK        : " + Build.VERSION.SDK_INT +
+                      "\nApp VersionName    : " + BuildConfig.VERSION_NAME +
+                      "\nApp VersionCode    : " + BuildConfig.VERSION_CODE +
+                      "\n<<<<<<<<<<<<<<<< File Header <<<<<<<<<<<<<<<<\n\n";
+              appendLog(header);
+            }
+          })
+          .build();
+  
+      XLog.init(                                                 // Initialize XLog
+          config,                                                // Specify the log configuration, if not specified, will use new LogConfiguration.Builder().build()
+          androidPrinter,                                        // Specify printers, if no printer is specified, AndroidPrinter(for Android)/ConsolePrinter(for java) will be used.
+          filePrinter);
+  
+      // For future usage: partial usage in MainActivity.
+      // globalFilePrinter = filePrinter;
+  
+      // Intercept all logs(including logs logged by third party modules/libraries) and print them to file.
+      // LibCat.config(true, filePrinter);
     }
 }
